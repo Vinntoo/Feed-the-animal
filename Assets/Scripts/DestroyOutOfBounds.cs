90ea966524659863bce07f5ff9909abdc800479b
Vincent Kiptoo Cheruiyot <37868729+Vinntoo@users.noreply.github.com>
2022-08-28 18:48:39 +0300

    Gameplay Fix

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30.0f;
-    private float lowerBound = -10.0f;
+    private float topBound = 30;
+    private float lowerBound = -10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // If an object goes past the players view in the game, remove 
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
-       
+
     }
 }
